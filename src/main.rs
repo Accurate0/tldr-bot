713b771b25227b9c0a92dbf14bfa6db4e21f80c7
Anurag Singh <contact@anurag.sh>
2024-09-29 17:03:01 +0800

    4h default

--- src/main.rs
+++ src/main.rs
@@ -264,13 +264,13 @@ fn parse_datetime_str(s: &str) -> anyhow::Result<TimeDelta> {
 #[error_handler(handle_interaction_error)]
 async fn summarise(
     ctx: &mut SlashContext<BotContext>,
-    #[description = "how far to go back in summary (default: 2h)"] timeframe: Option<String>,
+    #[description = "how far to go back in summary (default: 4h)"] timeframe: Option<String>,
     #[description = "only show me the message"] ephemeral: Option<bool>,
 ) -> DefaultCommandResult {
     let ephemeral = ephemeral.unwrap_or(false);
     ctx.defer(ephemeral).await?;
 
-    let from = parse_datetime_str(&timeframe.unwrap_or("2h".to_string()))?;
+    let from = parse_datetime_str(&timeframe.unwrap_or("4h".to_string()))?;
 
     let from = chrono::offset::Utc::now()
         .naive_utc()
